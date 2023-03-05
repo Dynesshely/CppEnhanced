9ff4df8ff1609be22954978dd83a729bf5f040c0
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2023-03-05 10:28:32 +0800

    📄 🔧 🎇 📝 📦 Docs, Fix, Style, Chore, Struct: Perfect the project     1.修改了README.md，添加How to build     2.修复了无法在Linux平台编译的问题     3.修改了CMakeLists.txt     4.source改名为src,符合国际惯例     5.public/std.h改名为stdc++.h,遵守GPL许可证     6.修改.gitignore     7.添加libplatform作为平台判断     具体内容: 	modified:   .gitignore 	modified:   CMakeLists.txt 	deleted:    CMakeSettings.json 	modified:   README.md 	new file:   include/cppfecs.hpp 	deleted:    include/datetime.h 	new file:   include/datetime.hpp 	new file:   include/libplatfrom.h 	deleted:    library.cpp 	deleted:    library.h 	renamed:    public/std.h -> public/stdc++.h 	deleted:    source/datetime.cpp 	new file:   src/datetime.cpp

---
+++ include/cppfecs.hpp
@@ -0,0 +1,58 @@
+#ifndef _INC_STDCPP_FECS_HPP_
+#define _INC_STDCPP_FECS_HPP_
+#include "libplatfrom.h"
+
+#ifndef __has_include
+#   define __has_include(x) 1
+#endif
+
+#ifndef _CONSTEXPR11
+#   if __has_cpp11__
+#       define _CONSTEXPR11 constexpr
+#   else
+#       define _CONSTEXPR11
+#   endif
+#endif
+
+#ifndef _CONSTEXPR14
+#   if __has_cpp14__
+#      define _CONSTEXPR14 constexpr
+#   else
+#       define _CONSTEXPR14
+#   endif
+#endif
+
+#ifndef _CONSTEXPR17
+#   if __has_cpp17__
+#       define _CONSTEXPR17 constexpr
+#   else
+#       define _CONSTEXPR17
+#   endif
+#endif
+
+#ifndef _CONSTEXPR20
+#   if __has_cpp20__
+#       define _CONSTEXPR20 constexpr
+#   else
+#       define _CONSTEXPR20
+#   endif
+#endif
+
+#ifndef __declspec
+#if !__has_msvc_compiler__
+#   define __declspec(x)
+#endif
+#endif
+
+#if !__has_cpp11__
+#   define constexpr
+#   define noexcept
+#endif
+
+#if !__has_cpp17__
+#   define __nodiscard__ 
+#else
+#   define __nodiscard__ [[nodiscard]]
+#endif
+
+#endif
--- library.h
+++
@@ -1,6 +0,0 @@
-#ifndef CPPENHANCED_LIBRARY_H
-#define CPPENHANCED_LIBRARY_H
-
-void hello();
-
-#endif //CPPENHANCED_LIBRARY_H
